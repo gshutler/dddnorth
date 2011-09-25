8d3e5e600b23acf133345052f8c6227dda6c06d6
Garry Shutler <garry@robustsoftware.co.uk>
2011-09-25 12:50:29 +0100

    Added separate views for adding and editing tasks to demonstrate routing.

--- static/application.js
+++ static/application.js
@@ -1,5 +1,25 @@
 (function() {
     
+    _.extend(Backbone.View.prototype, {
+    	
+    	show : function() {
+    		this._show();
+    	},
+    	
+    	hide : function() {
+    		this._hide();
+    	},
+    	
+    	_show : function() {
+    		$(this.el).show();
+    	},
+    	
+    	_hide : function() {
+    		$(this.el).hide();
+    	}
+    
+    });
+    
     var Task = Backbone.Model.extend({
     
 		initialize : function() {
@@ -50,6 +70,46 @@
     
     });
     
+    var TaskEditView = Backbone.View.extend({
+    	    	
+    	initialize : function() {
+    		this.form = this.$(".edit")[0];
+    		
+    		_.bindAll(this, "render", "saveTask", "deleteTask");
+    	},
+    	
+    	events : {
+    	
+    		"click .save-task" : "saveTask",
+    		
+    		"click .delete-task" : "deleteTask"
+    	
+    	},
+    	
+    	render : function(model) {
+    		this.model = model;
+        	this.form.name.value = model.get("name");
+        	this.form.description.value = model.get("description");
+        	            
+            return this;
+    	},
+    	
+    	saveTask : function() {        	
+        	var json = {
+        		"name" : this.form.name.value,
+        		"description" : this.form.description.value
+        	};
+        	
+        	this.model.set(json);
+        	this.model.save();
+        },
+        
+        deleteTask : function() {
+        	this.model.destroy();
+        }        
+    	
+    });
+    
     var TaskView = Backbone.View.extend({
     
     	className : "task",
@@ -58,15 +118,7 @@
     	
     		"click .move-left" : "moveLeft",
     		
-    		"click .move-right" : "moveRight",
-    		
-    		"click .display" : "toggleExpand",
-    		
-    		"dblclick .display" : "beginEdit",
-    		
-    		"click .cancel-edit" : "endEdit",
-    		
-    		"click .save-task" : "saveTask"
+    		"click .move-right" : "moveRight"
     	
     	},
 
@@ -76,36 +128,6 @@
             $(this.el).html(html);
             
             return this;
-        },
-        
-        toggleExpand : function() {
-        	$(this.el).toggleClass("expanded");
-        },
-        
-        beginEdit : function() {
-        	$(this.el).addClass("edit");
-        	this.$(".name").focus();
-        },
-        
-        endEdit : function() {
-        	this.render();
-        	$(this.el).removeClass("edit");
-        	return false;
-        },
-        
-        saveTask : function() {
-        	var form = this.$(".edit")[0];
-        	
-        	var json = {
-        		"name" : form.name.value,
-        		"description" : form.description.value
-        	};
-        	
-        	this.model.set(json);
-        	this.model.save();        	
-        	this.endEdit();
-        	
-        	return false;
         },
         
         moveLeft : function() {
@@ -130,43 +152,35 @@
     
     var AddTaskView = Backbone.View.extend({
     
+    	initialize : function() {
+    		this.form = this.$("form.add")[0];
+    		
+    		_.bindAll(this, "reset", "addTask");
+    	},
+    
     	events : {
-    	
-    		"click #show-add-form" : "showForm",
-    		
-    		"click .cancel" : "reset",
-    		
+    		    		
     		"click .add-task" : "addTask"
     	
     	},
     
-    	addTask : function() {
-    		var form = this.$("form.add")[0];
-        	
+    	addTask : function() {        	
         	var json = {
-        		"name" : form.name.value,
-        		"description" : form.description.value
+        		"name" : this.form.name.value,
+        		"description" : this.form.description.value
         	};
         	
-        	this.collection.create(json);        	
-        	this.reset();
-        	
-        	return false;
+        	this.collection.create(json);
     	},
     	
-    	showForm : function() {
-    		$(this.el).addClass("show-form");
+    	show : function() {    		
+    		this._show();
     		this.$(".name").focus();
     	},
     
     	reset : function() {
-    		$(this.el).removeClass("show-form");
-    		var form = this.$("form.add")[0];
-    		
-    		form.name.value = "";
-    		form.description.value = "";
-    		
-    		return false;
+    		this.form.name.value = "";
+    		this.form.description.value = "";
     	}
     
     });
@@ -179,7 +193,6 @@
             this.todoColumnView = new TaskListColumnView({ collection : this.collection, el : "#todo", status : "todo" });
             this.doingColumnView = new TaskListColumnView({ collection : this.collection, el : "#doing", status : "doing" });
             this.doneColumnView = new TaskListColumnView({ collection : this.collection, el : "#done", status : "done" });
-            this.addTaskView = new AddTaskView({ collection : this.collection, el : "#add-task" });
             
             this.collection.bind("add", this.render);
             this.collection.bind("remove", this.render);            
@@ -221,15 +234,44 @@
     var TodoList = Backbone.Router.extend({
 
         initialize : function(options) {
-            this.todoListView = new TaskListView({ collection : options.tasks });
+        	this.tasks = options.tasks;
+            this.todoListView = new TaskListView({ collection : options.tasks, el : "#todo-list" });
+			this.addTaskView = new AddTaskView({ collection : options.tasks, el : "#add-task" });
+			this.taskEditView = new TaskEditView({ el : "#edit-task" });
         },
     
         routes : {
-            "" : "index"
+            
+            "" : "index",
+            
+            "task/new" : "newTask",
+            
+            "task/:id" : "showTask"
+                        
         },
     
         index : function() {
+        	this._hideAll();
             this.todoListView.render();
+            this.todoListView.show();
+        },
+        
+        showTask : function(id) {
+	        this._hideAll();
+        	this.taskEditView.render(this.tasks.get(id));
+        	this.taskEditView.show();
+        },
+        
+        newTask : function() {
+        	this._hideAll();
+        	this.addTaskView.reset();
+        	this.addTaskView.show();
+        },
+        
+        _hideAll : function() {
+        	this.todoListView.hide();
+        	this.addTaskView.hide();
+        	this.taskEditView.hide();
         }
 
     });
